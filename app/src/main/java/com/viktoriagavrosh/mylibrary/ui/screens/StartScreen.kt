26674786fb3b0f278c385d85d2720e64fa0a01cd
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2024-01-12 18:13:45 +0300

    delete button on StartScreen add navigation with Enter on keyboard

--- app/src/main/java/com/viktoriagavrosh/mylibrary/ui/screens/StartScreen.kt
+++ app/src/main/java/com/viktoriagavrosh/mylibrary/ui/screens/StartScreen.kt
@@ -3,7 +3,8 @@ package com.viktoriagavrosh.mylibrary.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.viktoriagavrosh.mylibrary.R
@@ -37,18 +39,17 @@ fun StartScreen(
         )
         OutlinedTextField(
             value = libraryUiState.textQuery,
+            singleLine = true,
             onValueChange = onTextFieldChange,
             modifier = Modifier
-                .padding(vertical = dimensionResource(id = R.dimen.start_screen_vertical_padding))
-        )
-        Button(
-            onClick = onButtonClick
-        ) {
-            Text(
-                text = stringResource(R.string.ok),
-                style = MaterialTheme.typography.displaySmall
+                .padding(vertical = dimensionResource(id = R.dimen.start_screen_vertical_padding)),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Go
+            ),
+            keyboardActions = KeyboardActions(
+                onGo = { onButtonClick() }
             )
-        }
+        )
     }
 }
 
